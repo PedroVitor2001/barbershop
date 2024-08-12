e1b22fd2e24e1d32aac1128c577ba582e12e6ba5
Pedro Petrilli <ppetrilli96@gmail.com>
2024-08-12 14:53:13 -0300

    feat: ensure bookings are listed on bookings page

---
+++ app/bookings/page.tsx
@@ -0,0 +1,76 @@
+import React from "react"
+import Header from "../_components/header"
+import { db } from "../_lib/prisma"
+import { getServerSession } from "next-auth"
+import { authOptions } from "../_lib/auth"
+import { notFound } from "next/navigation"
+import BookingItem from "../_components/booking-item"
+import { lt } from "date-fns/locale"
+
+const Bookings = async () => {
+  const session = await getServerSession(authOptions)
+  if (!session?.user) {
+    return notFound()
+  }
+
+  const confirmedBookings = await db.booking.findMany({
+    where: {
+      userId: (session.user as any).id,
+      date: {
+        gte: new Date(),
+      },
+    },
+    include: {
+      service: {
+        include: {
+          barbershop: true,
+        },
+      },
+    },
+    orderBy: {
+      date: "asc",
+    },
+  })
+
+  const concluedBookings = await db.booking.findMany({
+    where: {
+      userId: (session.user as any).id,
+      date: {
+        lt: new Date(),
+      },
+    },
+    include: {
+      service: {
+        include: {
+          barbershop: true,
+        },
+      },
+    },
+    orderBy: {
+      date: "asc",
+    },
+  })
+
+  return (
+    <>
+      <Header />
+      <div className="space-y-3 p-5">
+        <h1 className="text-xl font-bold">Agendamentos</h1>
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Confirmados
+        </h2>
+        {confirmedBookings.map((booking) => (
+          <BookingItem key={booking.id} booking={booking} />
+        ))}
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Finalizados
+        </h2>
+        {concluedBookings.map((booking) => (
+          <BookingItem key={booking.id} booking={booking} />
+        ))}
+      </div>
+    </>
+  )
+}
+
+export default Bookings
